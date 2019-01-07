c8534ea15776b7f23c3715b1ff02ca6c3cc5e999
andreas <ksc91u@gmail.com>
2019-01-07 15:21:39 +0800

    call grpc service

--- app/src/main/java/example/com/myapplication/MainActivity.kt
+++ app/src/main/java/example/com/myapplication/MainActivity.kt
@@ -1,6 +1,13 @@
 package example.com.myapplication
 
 import android.os.Bundle
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import io.grpc.Server
+import io.grpc.ServerBuilder
+import io.grpc.examples.helloworld.GreeterGrpc
+import io.grpc.examples.helloworld.HelloReply
+import io.grpc.examples.helloworld.HelloRequest
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.get
@@ -8,6 +15,9 @@ import org.koin.android.ext.android.get
 class MainActivity : BaseActivity() {
 
     private lateinit var mainViewModel: MainViewModel
+
+    private lateinit var managedChannel: ManagedChannel
+    private lateinit var blockingStub: GreeterGrpc.GreeterBlockingStub
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +32,28 @@ class MainActivity : BaseActivity() {
             (application as KoinApplication).refreshScope()
             this@MainActivity.refresh()
         }
+
+        grpc()
+    }
+
+    fun grpc() {
+        managedChannel = ManagedChannelBuilder.forAddress("artifactory.ksc91u.info", 50051)
+            .usePlaintext()
+            .build()
+
+        blockingStub = GreeterGrpc.newBlockingStub(managedChannel)
+
+        val request = HelloRequest.newBuilder().setName("johnny").build()
+        var response: HelloReply? = null
+
+        try{
+            response = blockingStub.sayHello(request)
+        }catch (exception : Exception){
+            println(">>>> exception $exception")
+        }
+
+        println(">>>> response ${response?.message}")
+
     }
 
     fun refresh() {
